757ffa4cc33a3351bbd28d1dc2c3e56cb6c0db78
vasco <vastur@gmail.com>
2020-03-16 02:24:21 -0700

    Refactor rollup bundle config

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,77 @@ import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import { terser } from "rollup-plugin-terser";
+import dts from 'rollup-plugin-dts';
+import { name, homepage, version, dependencies } from './package.json';
 
-export default {
-  external: ['three'],
-  input: 'src/index.js',
-  output: [
-    {
-      globals: { three: 'THREE' }, // a-frame exposes three as global
-      strict: false, // a-frame 0.7 not 'strict' compatible
-      format: 'umd',
-      name: 'ForceGraphVR',
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    postCss({
-      plugins: [
-        postCssSimpleVars(),
-        postCssNested()
-      ]
-    }),
-    babel({ exclude: 'node_modules/**' }),
-    resolve(),
-    commonJs()
-  ]
+const umdConf = {
+  globals: { three: 'THREE' }, // a-frame exposes three as global
+  strict: false, // a-frame 0.7 not 'strict' compatible
+  format: 'umd',
+  name: 'ForceGraphVR',
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  {
+    external: ['three'],
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonJs()
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel()
+    ]
+  },
+  { // expose TS declarations
+    input: 'src/index.d.ts',
+    output: [{
+      file: `dist/${name}.d.ts`,
+      format: 'es'
+    }],
+    plugins: [dts()]
+  }
+];
